70b12cdc0123ffc414c3450754c50027e16dcf54
garcelomarcia <marcelogarcia91@gmail.com>
2025-09-17 14:45:23 -0600

    fourth commit

--- tienda_proj/settings.py
+++ tienda_proj/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ],
 
